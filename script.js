2661add2aa710038c7407c8ce6c21a439ec9c748
Maxence POUTORD <maxence.poutord@gmail.com>
2017-01-23 22:47:39 +0000

    :shit: new script

---
+++ script.js
@@ -0,0 +1,20 @@
+
+sleep = (ms) {
+  return new Promise(r => setTimeout(r, ms))
+},
+
+delay(100 )
+     .then(function STEP2(){
+        console.log("step 2 (after 100ms)");
+        return delay(200 );
+    })
+    .then(function STEP3(){
+        console.log("step 3 (after another 200ms)" );
+    })
+    .then(function STEP4(){   
+        console.log("step 4 (next Job)");
+        return delay( 50 );
+    })  
+    .then(function STEP5(){
+        console.log("step 5 (after another 55ms)" );
+    })  
